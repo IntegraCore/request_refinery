e9bc01593830806681823205827629aadbeac563
Chris Iriondo <ciriondo@integracore.com>
2015-02-12 15:01:16 +0000

    Another fix attempt.

--- app/controllers/request_refinery/application_controller.rb
+++ app/controllers/request_refinery/application_controller.rb
@@ -1,6 +1,6 @@
 module RequestRefinery
   class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
+	before_action :authenticate_request_refinery_user!
 	before_action :enforce_request_permissions, except: [:unauthorized_request, :users]
 
 	helper_method :authorized_to?
