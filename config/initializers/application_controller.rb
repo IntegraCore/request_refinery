4c03a1f6ac360b80d5c574b1944c9233418365c7
Chris Iriondo <ciriondo@integracore.com>
2015-02-12 15:06:21 +0000

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA.

--- config/initializers/application_controller.rb
+++ config/initializers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  	before_action :authenticate_user!
+  	before_action :authenticate_request_refinery_user!
   	before_action :enforce_request_permissions, except: [:unauthorized_request]
 
 	helper_method :authorized_to?
